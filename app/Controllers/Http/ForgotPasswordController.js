9626e0da1e1d4a4442318de314ba062a0d8e3ac9
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-28 17:39:53 -0300

    Created resetPassword.

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
+
 const User = use('App/Models/User')
 const Mail = use('Mail')
+
 class ForgotPasswordController {
   async store ({ request, response }) {
     try {
@@ -28,6 +31,36 @@ class ForgotPasswordController {
             .subject('Recuperação de Senha')
         }
       )
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Email não existe' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'Token de recuperação está expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+
+      return user
     } catch (error) {
       return response
         .status(error.status)

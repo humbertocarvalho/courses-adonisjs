cefb447b73a5dcd16ac3ab5f987bb089dab704f7
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-30 18:08:23 -0300

    Add validators to the routes.

--- start/routes.js
+++ start/routes.js
@@ -20,14 +20,20 @@ Route.post('users', 'UserController.store').validator('User')
 
 Route.post('session', 'SessionController.store').validator('Session')
 
-Route.post('forgot', 'ForgotPasswordController.store')
-Route.put('forgot', 'ForgotPasswordController.update')
+Route.post('forgot', 'ForgotPasswordController.store').validator(
+  'ForgotPassword'
+)
+Route.put('forgot', 'ForgotPasswordController.update').validator(
+  'ResetPassword'
+)
 
 Route.get('files/:id', 'FileController.show')
 Route.group(() => {
   Route.post('files', 'FileController.store')
 
-  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store'], ['Project']]]))
 
   Route.resource('projects.tasks', 'TaskController').apiOnly()
 }).middleware(['auth'])

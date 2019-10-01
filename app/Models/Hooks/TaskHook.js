34c41a0a4943e1b6bcd17b5dd07e6d8c5bbc9543
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-30 21:48:05 -0300

    Add redis queue.

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const NewTaskMailJob = use('App/Jobs/NewTaskMail')
+
 const TaskHook = (exports = module.exports = {})
 
 TaskHook.sendNewTaskMail = async taskInstance => {
@@ -11,25 +12,11 @@ TaskHook.sendNewTaskMail = async taskInstance => {
     const file = await taskInstance.file().fetch()
 
     const { title } = taskInstance
-    await Mail.send(
-      ['emails.new_task'],
-      {
-        username,
-        title,
-        hasAttachment: !!file
-      },
-      message => {
-        message
-          .to(email)
-          .from('humberto@viezzer.com.br', 'Humberto')
-          .subject('Nova tarefa para você')
 
-        if (file) {
-          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-            filename: file.name
-          })
-        }
-      }
+    Kue.dispatch(
+      NewTaskMailJob.key,
+      { email, username, file, title },
+      { attempts: 3 }
     )
   }
 }

0ce22effa5fdb40252fce7ca00aa29f8fb8d56c2
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-01 18:44:20 -0300

    Add permissions and roles to the user.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,15 +4,35 @@ const User = use('App/Models/User')
 
 class UserController {
   async store ({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+    const { permissions, roles, ...data } = request.only([
+      'username',
+      'email',
+      'password',
+      'permissions',
+      'roles'
+    ])
     const addresses = request.input('addresses')
 
     const trx = await Database.beginTransaction()
 
     const user = await User.create(data, trx)
-    await user.addresses().createMany(addresses, trx)
+
+    if (addresses) {
+      await user.addresses().createMany(addresses, trx)
+    }
 
     await trx.commit()
+
+    if (roles) {
+      await user.roles().attach(roles)
+    }
+
+    if (permissions) {
+      await user.permissions().attach(permissions)
+    }
+
+    await user.loadMany(['roles', 'permissions'])
+
     return user
   }
 }
